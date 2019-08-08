23ffc324a090c3e95da34ac14b2b3786d8bc254b
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-08-08 16:17:45 +0200

    api repos

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
-//var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
+
 var bodyParser = require('body-parser');
 var request = require('request');
 var env = require('dotenv')
 var githubWebhook = require('./src/githubWebhook');
 var cors = require('cors')
-
-var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 app.use(bodyParser.json());
@@ -20,45 +15,10 @@ app.use(cors());
 
 var corsOptions = {
   origin: 'https://lab9k.gent',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200
 }
 
 app.get('/members',cors(corsOptions), githubWebhook.getMembers);
-
- 
-
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//app.use(logger('dev'));
-
-//app.use(cookieParser());
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-/*
-const PORT = process.env.PORT || 2900;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});*/
+app.get('/repos',cors(corsOptions), githubWebhook.getRepos);
 
 module.exports = app;
--- src/githubWebhook.js
+++ src/githubWebhook.js
@@ -18,4 +18,21 @@ module.exports= {
         });
 
     },
+    getRepos(req,res)
+    {
+
+        var options = {
+            uri: 'https://api.github.com/orgs/lab9k/repos',
+            headers: {
+              'User-agent':'henrivdb',
+              Authorization: 'token '+ process.env.GITHUB_PERSONAL_ACCESS_TOKEN,
+          }
+        };
+
+        request.get(options, function (error, response, body) {
+          res.status(200).send(body);
+          console.log('error:', error); // Print the error if one occurred
+        });
+
+    },
 };
