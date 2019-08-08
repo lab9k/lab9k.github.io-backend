a44e9dbd8289448ea7613649e4c3ef9eaa9dd46d
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-08-08 15:08:39 +0200

    send members

--- src/githubWebhook.js
+++ src/githubWebhook.js
@@ -1,7 +1,7 @@
 var request = require('request');
 
 module.exports= {
-    getMembers()
+    getMembers(req,res)
     {
 
         var options = {
@@ -13,6 +13,7 @@ module.exports= {
         };
 
         request.get(options, function (error, response, body) {
+          res.status(200).send(body);
           console.log('error:', error); // Print the error if one occurred
           console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
           console.log('body:', body); // Print the HTML for the Google homepage.
