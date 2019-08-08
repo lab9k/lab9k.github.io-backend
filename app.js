80d8b6690ebd2f11c3b8893530d2a6c34b3d3ac8
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-08-08 14:50:02 +0200

    app

--- app.js
+++ app.js
@@ -12,21 +12,22 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.get('/members', githubWebhook.getMembers);
+env.config();
 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
 
-env.config();
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 //app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/members', githubWebhook.getMembers);
+//app.use(cookieParser());
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -46,7 +47,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2900;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
