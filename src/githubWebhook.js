0d55087d35b49c4bec29eed29c4a40910887fc7d
Henri Van den Braembussche <henri.vandenbraembussche.w1475@student.hogent.be>
2019-08-08 15:24:18 +0200

    return JSON

--- src/githubWebhook.js
+++ src/githubWebhook.js
@@ -13,10 +13,11 @@ module.exports= {
         };
 
         request.get(options, function (error, response, body) {
-          res.status(200).send(body);
+          res.status(200).send(JSON(body));
           console.log('error:', error); // Print the error if one occurred
           console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
           console.log('body:', body); // Print the HTML for the Google homepage.
         });
+
     },
 };
